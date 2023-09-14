ccabb161d8ccf381a4fe2740df02fd2d42e3da47
s.alekseev <s.alekseev@hypeauditor.com>
2023-09-14 08:19:20 +0300

    init

---
+++ wp-content/themes/ma/functions.php
@@ -0,0 +1,90 @@
+<?php
+add_action( 'after_setup_theme', 'blankslate_setup' );
+function blankslate_setup()
+{
+    add_theme_support( 'title-tag' );
+    add_theme_support( 'automatic-feed-links' );
+    add_theme_support( 'post-thumbnails' );
+}
+add_action( 'wp_enqueue_scripts', 'blankslate_load_scripts' );
+function blankslate_load_scripts()
+{
+    wp_enqueue_script( 'jquery' );
+    wp_enqueue_script( 'jquery_ui', get_bloginfo('template_url').'/lib/jquery-ui.min.js', array('jquery'), '1.0', true );
+    wp_enqueue_script( 'punch', get_bloginfo('template_url').'/lib/jquery.ui.touch-punch.min.js', array('jquery_ui'), '1.0', true );
+    wp_enqueue_script( 'mamain', get_bloginfo('template_url').'/main.js', array('punch'), '1.0', true );
+}
+add_action( 'comment_form_before', 'blankslate_enqueue_comment_reply_script' );
+function blankslate_enqueue_comment_reply_script()
+{
+    if ( get_option( 'thread_comments' ) ) { wp_enqueue_script( 'comment-reply' ); }
+}
+add_filter( 'the_title', 'blankslate_title' );
+function blankslate_title( $title ) {
+    if ( $title == '' ) {
+        return '&rarr;';
+    } else {
+        return $title;
+    }
+}
+add_filter( 'wp_title', 'blankslate_filter_wp_title' );
+function blankslate_filter_wp_title( $title )
+{
+    return $title . esc_attr( get_bloginfo( 'name' ) );
+}
+add_filter( 'get_comments_number', 'blankslate_comments_number' );
+function blankslate_comments_number( $count )
+{
+    if ( !is_admin() ) {
+        global $id;
+        $comments_by_type = &separate_comments( get_comments( 'status=approve&post_id=' . $id ) );
+        return count( $comments_by_type['comment'] );
+    } else {
+        return $count;
+    }
+}
+add_filter( 'show_admin_bar', '__return_false' );
+
+add_action('init', 'anew_rewrite_rule');
+function anew_rewrite_rule(){
+    add_rewrite_rule('^kredit\\/(.*)','index.php?is_generated_page=1','top');
+}
+
+add_action('query_vars','controller_set_query_var');
+function controller_set_query_var($vars) {
+    array_push($vars, 'is_generated_page'); // ref url redirected to in add rewrite rule
+
+    return $vars;
+}
+
+add_filter('template_include', 'include_controller');
+function include_controller($template){
+    if(get_query_var('is_generated_page')){
+        $new_template = get_stylesheet_directory().'/page-calc.php';
+        if(file_exists($new_template)){
+            $template = $new_template;
+
+        }
+    }
+    return $template;
+}
+
+if (function_exists('acf_add_options_page')) {
+    acf_add_options_page();
+}
+
+function get_calc_template() {
+    $template = [];
+    while ( have_rows( 'filter', 'option' ) ) : the_row();
+        $filter_label = get_sub_field( 'filter_label' );
+        while ( have_rows( 'filter_options', 'option' ) ) : the_row();
+            $filter_option_label = get_sub_field( 'filter_option_label' );
+            $filter_option_slug = get_sub_field( 'filter_option_slug' );
+            $filter_option_template = get_sub_field( 'filter_option_template' );
+            if (strpos($_SERVER['REQUEST_URI'], $filter_option_slug)) {
+                $template[] = $filter_option_template;
+            }
+        endwhile;
+    endwhile;
+    return implode(' ', $template);
+}
---
+++ wp-content/themes/ma/main.js
@@ -0,0 +1,162 @@
+jQuery(function($){
+    window.getPercent = () => {
+        if ($('[name="kreditnaya-istoriya"]').val() === 'bez-kreditnoy-istorii') {
+            return 16;
+        }
+        if (
+            $('[name="kreditnaya-istoriya"]').val() === 'horoshaya-kreditnaya-istoriya'
+            && $('[name="rabota"]').val() === 'so-spravkoy-o-dohodah'
+        ) {
+            return 14;
+        }
+        if (
+            $('[name="kreditnaya-istoriya"]').val() === 'horoshaya-kreditnaya-istoriya'
+            && $('[name="rabota"]').val() === 'bez-spravki-o-dohodah'
+        ) {
+            return 15;
+        }
+        if (
+            ['bez-kreditnoy-istorii', 'horoshaya-kreditnaya-istoriya'].indexOf($('[name="kreditnaya-istoriya"]').val()) === -1
+            && ['nedvijimost', 'kvartira', 'dom', 'kommercheskaya-nedvijimost'].indexOf($('[name="tip-zaloga"]').val()) !== -1
+        ) {
+            return 18;
+        }
+        return 20;
+    }
+    window.recount = () => {
+        const percent = getPercent();
+        $('.js-percent').html(percent);
+        const monthly = Math.round(window.sum * percent / 100 / 12 / (1 - Math.pow(1 + percent / 100 / 12, -window.duration)))
+        $('.js-monthly').html(monthly.toLocaleString('en-US').replace(/,/g, '&nbsp;'))
+//             `X=SxP/1-(1+p)x(1-m)
+// • X - ежемесячный платеж по кредиту, который и нужно рассчитать
+// • S - общий размер ипотеки
+// . Р - месячная процентная ставка (т.е. годовая ставка, которая разделена на 12 мес.)
+// • М - срок ипотечного кредитования (считается в месяцах)`
+    }
+//     `
+//     Нулевой клиент
+// Возраст от 21 до 65 + нет кредитной истории (никогда не брал кредиты) = 16% годовых, сумма 1 млн (срок пусть сам ставит, срок кредитования до 30 лет, но по возрасту не более 75)
+//
+// Хороший клиент
+// Возраст от 21 до 65 + хорошая кредитная история + есть доход + есть подтверждение дохода  = 14% годовых, сумма до 10 млн, срок также считаем
+//
+// Бридж клиент
+// Возраст от 21 до 65 + хорошая кредитная история + нет подтверждения дохода = 15% годовых, сумма до 10 млн
+//
+// Страйк клиент
+// Возраст от 21 до 65 + плохая кредитная история + есть недвижимость = 18% годовых, срок также
+//
+// При всех значениях неважно есть или нет недвижимость, созаемщик и тд, возьмем за основу кредитную историю, кроме последней программы. Там при наличии просрочек обязательно должна быть недвижимость.
+//
+// Если он вводит остальные параметры кроме истории и подтверждения дохода, то они не учитываются просто (не влияют на ставку)
+//     `
+    const priceVals = [
+        100000,
+        110000,
+        120000,
+        130000,
+        140000,
+        150000,
+        250000,
+        350000,
+        450000,
+        500000,
+        1000000,
+        2000000,
+        3000000,
+        4000000,
+        5000000,
+        6000000,
+        7000000,
+        8000000,
+        9000000,
+        10000000,
+    ]
+    const durationVals = [
+        {
+            name: 'На полгода',
+            value: 6,
+        },
+
+        {
+            name: 'На год',
+            value: 12,
+        },
+        {
+            name: 'На 2 года',
+            value: 2 * 12,
+        },
+        {
+            name: 'На 3 года',
+            value: 3 * 12,
+        },
+        {
+            name: 'На 4 года',
+            value: 4 * 12,
+        },
+        {
+            name: 'На 5 лет',
+            value: 5 * 12,
+        },
+        {
+            name: 'На 6 лет',
+            value: 6 * 12,
+        },
+        {
+            name: 'На 7 лет',
+            value: 7 * 12,
+        },
+        {
+            name: 'На 8 лет',
+            value: 8 * 12,
+        },
+        {
+            name: 'На 9 лет',
+            value: 9 * 12,
+        },
+        {
+            name: 'На 10 лет',
+            value: 10 * 12,
+        },
+        {
+            name: 'На 11 лет',
+            value: 11 * 12,
+        },
+        {
+            name: 'На 12 лет',
+            value: 12 * 12,
+        }
+    ]
+    window.duration = durationVals[0].value
+    window.sum = priceVals[0]
+    $('.slider-price').closest('.sliders__ui-slider').append($(`
+        <div class="js-slider-min-max"><div class="js-slider-min">${priceVals[0].toLocaleString('en-US').replace(/,/g, '&nbsp;')}</div><div class="js-slider-max">${priceVals[priceVals.length-1].toLocaleString('en-US').replace(/,/g, '&nbsp;')}</div></div>    
+    `))
+    $('.slider-price').slider({
+        value: 0,
+        min: 0,
+        max: priceVals.length - 1,
+        slide: function(event, ui) {
+            const value = ui.value
+            window.sum = priceVals[value]
+            $('.js-price-val').html(`${priceVals[value].toLocaleString('en-US').replace(/,/g, '&nbsp;')}&nbsp;руб`)
+            recount()
+        },
+    });
+    $('.slider-duration').closest('.sliders__ui-slider').append($(`
+        <div class="js-slider-min-max"><div class="js-slider-min">${durationVals[0].name}</div><div class="js-slider-max">${durationVals[durationVals.length-1].name}</div></div>    
+    `))
+    $('.slider-duration').slider({
+        value: 0,
+        min: 0,
+        max: durationVals.length - 1,
+        slide: function(event, ui) {
+            const value = ui.value
+            $('.js-duration-val').html(durationVals[value].name)
+            window.duration = durationVals[value].value
+            recount()
+        },
+    });
+    $('body').on('change', '.calculate__calculator select', recount)
+});
