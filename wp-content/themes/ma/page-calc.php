dba78cea85add4d69f23d4185d4da8d69b83fe36
Oleg_thesame <proskyrin.oleg@gmail.com>
2023-10-17 23:16:34 +0600

    {wip}

--- wp-content/themes/ma/page-calc.php
+++ wp-content/themes/ma/page-calc.php
@@ -2,7 +2,6 @@
 $filters = get_calc_filters();
 get_header();
 global $replace_img;
-global $replace_bullets;
 $style_head_offer = '';
 $style_head_offer_ctr = '';
 if ($replace_img) {
@@ -25,40 +24,20 @@ if ($replace_img) {
             ?>
         </h1>
         <ul class="head-offers__head-benefits">
-            <li data-bullet="0" data-default-content="<?=htmlentities('За <strong>1 день</strong>')?>" class="head-benefits__head-benefit">
-                <?php if (isset($replace_bullets[0])) {
-                    echo $replace_bullets[0];
-                } else { ?>
-                    За <strong>1 день</strong>
-                <?php } ?>
+            <li class="head-benefits__head-benefit">
+                За <strong>1 день</strong>
             </li>
-            <li data-bullet="1" data-default-content="<?=htmlentities('Ставка <strong>5%</strong>')?>" class="head-benefits__head-benefit">
-                <?php if (isset($replace_bullets[1])) {
-                    echo $replace_bullets[1];
-                } else { ?>
-                    Ставка <strong>5%</strong>
-                <?php } ?>
+            <li class="head-benefits__head-benefit">
+                Ставка <strong>5%</strong>
             </li>
-            <li data-bullet="2" data-default-content="<?=htmlentities('С любой <strong>кредитной историей</strong>')?>" class="head-benefits__head-benefit">
-                <?php if (isset($replace_bullets[2])) {
-                    echo $replace_bullets[2];
-                } else { ?>
-                    С любой <strong>кредитной историей</strong>
-                <?php } ?>
+            <li class="head-benefits__head-benefit">
+                С любой <strong>кредитной историей</strong>
             </li>
-            <li data-bullet="3" data-default-content="<?=htmlentities('Минимум <strong>документов</strong>')?>" class="head-benefits__head-benefit">
-                <?php if (isset($replace_bullets[3])) {
-                    echo $replace_bullets[3];
-                } else { ?>
-                    Минимум <strong>документов</strong>
-                <?php } ?>
+            <li class="head-benefits__head-benefit">
+                Минимум <strong>документов</strong>
             </li>
-            <li data-bullet="4" data-default-content="<?=htmlentities('Максимальная <strong>сумма кредита</strong>')?>" class="head-benefits__head-benefit">
-                <?php if (isset($replace_bullets[4])) {
-                    echo $replace_bullets[4];
-                } else { ?>
-                    Максимальная <strong>сумма кредита</strong>
-                <?php } ?>
+            <li class="head-benefits__head-benefit">
+                Максимальная <strong>сумма кредита</strong>
             </li>
         </ul>
         <a href="#" class="head-offer__offer-link button-animate btn-5">
@@ -860,14 +839,14 @@ if ($replace_img) {
     <?php if ( get_row_layout() == 'our_credit_programs' ) : ?>
 
 
-<section class="tariffs">
+    <section class="tariffs">
 
 
-			    <h2 <?php live_edit('page_content, flex-our_credit_programs--header', get_option('page_on_front')) ?> class="nzlg-title tariffs__trf-title">
-        <?php echo strip_tags( get_sub_field( 'header', get_option('page_on_front') ), ['strong'] ); ?>
-    </h2>
+		<h2 <?php live_edit('page_content, flex-our_credit_programs--header', get_option('page_on_front')) ?> class="nzlg-title tariffs__trf-title">
+            <?php echo strip_tags( get_sub_field( 'header', get_option('page_on_front') ), ['strong'] ); ?>
+        </h2>
 
-    <?php get_field('programs_page', 'option')"?>
+    
 
 
 
@@ -916,7 +895,7 @@ if ($replace_img) {
                 </li>
 				<?php endif; ?>
 								<?php endwhile; ?>
-<?php endif; ?>
+                <?php endif; ?>
 
 				</ul>
             <a href="#" class="tariff-item__btn-primary button-animate btn-5">
@@ -926,73 +905,15 @@ if ($replace_img) {
             </a>
         </div>
 
-		  <?php endwhile; ?>
-<?php endif; ?>
+                <?php endwhile; ?>
+        <?php endif; ?>
 
 
 
     </div> -->
 
-    <?php while ( have_rows( 'page_content2' ) ) : the_row(); ?>
-<?php if ( get_row_layout() == 'our_credit_programs' ) : ?>
+        
 
-
-    <div <?php live_edit('page_content2, flex-our_credit_programs--programm_car') ?> class="tariffs__tariff-wrap">
-
-        <?php if (have_rows("programm_car")) : ?>
-    	<?php while (have_rows("programm_car")) : the_row(); ?>
-
-
-
-               
-
-                <?php if (get_sub_field('color_card')) : ?>
-                <div class="<?php the_sub_field("color_card") ?> click-btn  btn-style1">
-                    <?php endif; ?>
-
-                    <div class="tariff-item__icon-shield"></div>
-                    <h3 class="tariff-item__tariff-header">
-                        <?php if (get_sub_field('programm_name')) : ?>
-                            <?php the_sub_field("programm_name") ?>
-                        <?php endif; ?>
-                    </h3>
-                    <div class="tariff-item__tariff-desc">
-                        <?php if (get_sub_field('programm_description')) : ?>
-                            <?php the_sub_field("programm_description") ?>
-                        <?php endif; ?>
-                    </div>
-                    <div class="tariff-item__list-title">
-                        <?php if (get_sub_field('programm_condition')) : ?>
-                            <?php the_sub_field("programm_condition") ?>
-                        <?php endif; ?>
-                    </div>
-                    <ul class="tariff-item__desc-list">
-                        <?php if (have_rows("programm_condition_ul")) : ?>
-                            <?php while (have_rows("programm_condition_ul")) : the_row(); ?>
-                                <?php if (get_sub_field('programm_condition_ul_li')) : ?>
-                                    <li class="desc-list__desc-item">
-                                        <?php the_sub_field("programm_condition_ul_li") ?>
-                                    </li>
-                                <?php endif; ?>
-                            <?php endwhile; ?>
-                        <?php endif; ?>
-                    </ul>
-                    <a href="#" class="tariff-item__btn-primary button-animate">
-                        <?php if (get_sub_field('button_text')) : ?>
-                            <?php the_sub_field("button_text") ?>
-                        <?php endif; ?>
-                    </a>
-                </div>
-
-            
-
-    <?php endwhile; ?>
-    <?php endif; ?>
-
-    </div>
-
-<?php endif; ?>
-<?php endwhile; ?>
 
 
 
@@ -1009,8 +930,8 @@ if ($replace_img) {
 	<?php endif; ?>
 <?php endwhile; ?>
 
-<?php the_field('our_credit_programs' , 'options'); ?>
 
+	
 
 
 
