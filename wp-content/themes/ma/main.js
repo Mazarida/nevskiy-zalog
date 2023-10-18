65df51e28fa12ea60760cfba6bade90a20e9a863
Oleg_thesame <proskyrin.oleg@gmail.com>
2023-10-18 12:34:14 +0600

    {mobal-js}

--- wp-content/themes/ma/main.js
+++ wp-content/themes/ma/main.js
@@ -233,9 +233,35 @@ jQuery(function($){
 
 
 /* -- start Скрипт попапов --*/
+   document.getElementById("open-modal-btn1").addEventListener("click", function() {
+       document.getElementById("my-modal").classList.add("open");
+    })
 
-let modalBtn = document.querySelectorAll('.callback-form__btn-primary');
-console.log(modalBtn);
-console.log(modalBtn);
+   
+
+    document.getElementById("open-modal-btn2").addEventListener("click", function() {
+       document.getElementById("my-modal").classList.add("open");
+    })
+
+    document.getElementById("close-my-modal-btn").addEventListener("click", function() {
+       document.getElementById("my-modal").classList.remove("open");
+    })
+
+    // Закрыть модальное окно при нажатии на Esc
+window.addEventListener('keydown', (e) => {
+    if (e.key === "Escape") {
+        document.getElementById("my-modal").classList.remove("open")
+    }
+});
+
+// Закрыть модальное окно при клике вне его
+document.querySelector("#my-modal .modal__box").addEventListener('click', event => {
+    event._isClickWithInModal = true;
+});
+document.getElementById("my-modal").addEventListener('click', event => {
+    if (event._isClickWithInModal) return;
+    event.currentTarget.classList.remove('open');
+});
+
 
 /* -- end Скрипт попапов  --*/
