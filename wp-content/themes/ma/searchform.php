4abd7c4fdccb29e062b35b106fa0a7cd55c8efd5
Oleg_thesame <proskyrin.oleg@gmail.com>
2023-10-19 14:59:54 +0600

    search

---
+++ wp-content/themes/ma/searchform.php
@@ -0,0 +1,10 @@
+<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ) ?>" >
+	<!-- <label class="screen-reader-text" for="s">Поиск: </label>
+	<input type="text" value="<?php echo get_search_query() ?>" name="s" id="s" />
+	<input type="submit" id="searchsubmit" value="найти" /> -->
+
+      <input type="text" placeholder="Поиск..." class="desktop-only head-bot__search-input screen-reader-text" for="s">
+      <input type="text" value="<?php echo get_search_query() ?>" name="s" id="s" />
+        <a href="#" class="head-bot__head-search-icon"></a>
+        <input type="submit" id="searchsubmit" class="head-bot__head-search-icon" value="найти" />
+</form>
