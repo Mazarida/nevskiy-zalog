2c61886e95806077d1798d910aea91bd87966fb4
shod-web <amondilloev@mail.ru>
2023-10-20 11:44:29 +0500

    Test for production

--- wp-content/themes/ma/searchform.php
+++ wp-content/themes/ma/searchform.php
@@ -1,3 +1,4 @@
+<?php /*
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ) ?>" >
 	<!-- <label class="screen-reader-text" for="s">Поиск: </label>
 	<input type="text" value="<?php echo get_search_query() ?>" name="s" id="s" />
@@ -10,4 +11,5 @@
 
 			
 	</div>
-</form>
+</form> 
+*/ ?>
